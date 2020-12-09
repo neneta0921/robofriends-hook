f661dc3487d4c2c099a75f226c9d91361aef16d8
neneta0921 <65237907+neneta0921@users.noreply.github.com>
2020-12-09 13:37:35 +0900

    create cardList

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'CopepFlink Eromancy',
+    username: 'Luminous',
+    email: 'eccaminous@yriantumn88.com',
+  },
+  {
+    id: 2,
+    name: 'Mulligatawny Falcade',
+    username: 'Aardwolf',
+    email: 'sanaZeze@osmetology.com',
+  },
+  {
+    id: 3,
+    name: 'MissdicBulgur Ruthful',
+    username: 'Epistaxis',
+    email: 'esterJarvey@rabecula.com',
+  },
+  {
+    id: 4,
+    name: 'Cheesecake Clewrazyy',
+    username: 'Badinage',
+    email: 'ostulator@uscious.com',
+  },
+  {
+    id: 5,
+    name: 'Glockenspiel Cheverel',
+    username: 'Acrimony',
+    email: 'henango@ippomania.com',
+  },
+  {
+    id: 6,
+    name: 'Rejigger Islandic',
+    username: 'Ceticide',
+    email: 'erstringe@uminous.com',
+  },
+  {
+    id: 7,
+    name: 'Phalanx Extravasate',
+    username: 'Chaussures',
+    email: 'raster@ellcote.com',
+  },
+  {
+    id: 8,
+    name: 'Cornucopia Geotechnics',
+    username: 'Lepadoid',
+    email: 'ardwolf@actometer.com',
+  },
+  {
+    id: 9,
+    name: 'Bloviate Tulchan',
+    username: 'Molinology',
+    email: 'elation@oodgegend335.com',
+  },
+  {
+    id: 10,
+    name: 'BatgirEschew Sternutation',
+    username: 'Luminous',
+    email: 'ariomete@agilenom07.com',
+  },
+];
