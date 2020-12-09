29e8be043e27d039d52eab01ddd38c4f1ea0dd0a
neneta0921 <65237907+neneta0921@users.noreply.github.com>
2020-12-09 11:16:09 +0900

    props

--- src/Hello.js
+++ src/Hello.js
@@ -2,15 +2,13 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './Hello.css';
 
-class Hello extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello World!</h1>
-        <p>Welcome to React</p>
-      </div>
-    );
-  }
-}
+const Hello = (props) => {
+  return (
+    <div className="f1 tc">
+      <h1>Hello World!</h1>
+      <p>{props.greeting}</p>
+    </div>
+  );
+};
 
 export default Hello;
