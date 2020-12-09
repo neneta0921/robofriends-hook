1016944a1cf0d8e4bd15778bbc969d99b622d23f
neneta0921 <65237907+neneta0921@users.noreply.github.com>
2020-12-09 10:09:47 +0900

    npm install -g create-react-app

--- src/App.js
+++ src/App.js
@@ -7,14 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Hello World
+          <br />
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
