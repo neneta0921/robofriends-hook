3482ac878d67a1bbf85777cd35bebe692c6fda91
neneta0921 <65237907+neneta0921@users.noreply.github.com>
2020-12-14 08:30:23 +0900

    yarn add redux-logger

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,45 +1,70 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
-// STATE >> props
+import { setSearchField } from '../actions';
 
-function App() {
-  const [robots, setRobots] = useState([])
-  const [searchField, setSearchField] = useState('')
-  const [count, setCount] = useState(0)
-
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then((response) => response.json())
-      .then((users) => {setRobots(users)});
-    console.log(count);
-  }, [count]) // only run if count change
-
-  const onSearchChange = (event) => {
-    setSearchField(event.target.value);
-  };
-
-  const filteredRobots = robots.filter((robot) => {
-    return robot.name.toLowerCase().includes(searchField.toLowerCase());
-  });
-  return !robots.length ? (
-    <h1>Loading</h1>
-  ) : (
-    <div className="tc">
-      <h1 className="f1">RoboFriends</h1>
-      <button onClick={() => setCount(count+1)}>Click Me!</button>
-      <SearchBox searchChange={onSearchChange} />
-      <Scroll>
-        <ErrorBoundry>
-          <CardList robots={filteredRobots} />
-        </ErrorBoundry>
-      </Scroll>
-    </div>
-  );
+// parameter state comes from index.js provider store state(rootReducers)
+const mapStateToProps = state => {
+  return {
+    searchField: state.searchRobot.searchField
+  }
 }
 
-export default App;
+// dispatch the DOM changes to call an action.
+// note mapStateToProps returns object,
+// mapDispatchToProps returns function
+// the function returns an object
+// then uses connect to change the data from reducers.
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+  }
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: []
+    };
+  }
+
+  componentDidMount() {
+    console.log(this.props.store.getState());
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
+  }
+
+  render() {
+    const { robots } = this.state;
+    const { searchField, onSearchChange } = this.props;
+    const filteredRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
+    });
+    return !robots.length ? (
+      <h1>Loading</h1>
+    ) : (
+      <Fragment>
+        <div className="tc">
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox searchChange={onSearchChange} />
+          <Scroll>
+            <ErrorBoundry>
+              <CardList robots={filteredRobots} />
+            </ErrorBoundry>
+          </Scroll>
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+// action done from mapDispatchToProps will channge state from mapStateToProps
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/containers/app-react-hook.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
+import './App.css';
+
+// STATE >> props
+
+function App() {
+  const [robots, setRobots] = useState([])
+  const [searchField, setSearchField] = useState('')
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    console.log(this.porps.store)
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => {setRobots(users)});
+    console.log(count);
+  }, [count]) // only run if count change
+
+  const onSearchChange = (event) => {
+    setSearchField(event.target.value);
+  };
+
+  const filteredRobots = robots.filter((robot) => {
+    return robot.name.toLowerCase().includes(searchField.toLowerCase());
+  });
+  return !robots.length ? (
+    <h1>Loading</h1>
+  ) : (
+    <div className="tc">
+      <h1 className="f1">RoboFriends</h1>
+      <button onClick={() => setCount(count+1)}>Click Me!</button>
+      <SearchBox searchChange={onSearchChange} />
+      <Scroll>
+        <ErrorBoundry>
+          <CardList robots={filteredRobots} />
+        </ErrorBoundry>
+      </Scroll>
+    </div>
+  );
+}
+
+export default App;
